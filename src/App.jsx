712b7ff14266f7eb6d7a3ec97e037ad184ab5e4f
lucia nambi <nambilucia@gmail.com>
2024-07-09 21:21:24 +0300

    add responsiveness to hero page

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import Hero from "./Components/Hero";
 import Todolist from "./Components/Todolist";
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Home from './Components/Home';
 
 
 export default function App() {
   return (
     <div>
 
-        <Hero />
-
-        <Todolist />
+     
         <Router>
           <Routes>
+
             <Route path="/todolist" element={<Todolist />} />
+
+            <Route path="/" element={<Home />} />
+
           </Routes>
 
 
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { FaAngleDoubleUp } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import Todolist from './Todolist';
 
  function Hero() {
+  
   return (
     <div>
-      <div className='bg-[#d44dbb]'>
+      <div className='bg-[#d44dbb] w-full h-screen p-6 md:p-14'>
+        <h1 className='font-Poppins text-2xl md:text-4xl font-bold text-center'>TODO LIST<br />WELCOME BACK!!</h1>
+        <div className='m-10 md:m-20'>
+
+            <h2 className='font-Poppins text-xl md:text-3xl text-center font-medium leading-loose'>It's been a while,<br /> Swipe Up to Start your day <br />
+        </h2>
+        </div>
+      
+        <div>
+          <Link to="/Todolist">
+          
+        <FaAngleDoubleUp className='animate-bounce text-8xl text-center mx-auto m-20'/>  
+
+          </Link>
+
+        </div>
+      
+        
+
+
 
       </div>
     </div>
